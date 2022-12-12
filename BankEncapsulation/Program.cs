c42722dd7b0d624ecc1a64f886017a4f616a6c5f
nathan stephens <nstephens1234567@gmail.com>
2022-12-12 12:35:37 -0600

    initial commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount Checking = new BankAccount();
+            Console.WriteLine("How much money do you want to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            Checking.Deposit(amountToDeposit);
+            double userBalance = Checking.GetBalance();
+
+            Console.WriteLine($"Your current balance is: {userBalance, 0:c}");
+
         }
     }
 }
